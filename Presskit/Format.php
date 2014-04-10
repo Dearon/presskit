d224d14c06e181533243b7eb2f401c198ee4c06e
Dearon <dearon@gmail.com>
2014-04-10 17:43:10 +0200

    Changed the format class to generate urls and embedded html for trailers Changed the tests to test for the trailer function

--- Presskit/Format.php
+++ Presskit/Format.php
@@ -20,6 +20,10 @@ class Format
             $data['website-name'] = $this->url($data['website']);
         }
 
+        if (array_key_exists('trailers', $data)) {
+            $data['trailers'] = $this->trailers($data['trailers']);
+        }
+
         return $data;
     }
 
@@ -34,5 +38,24 @@ class Format
         }
 
         return $name;
+    }
+
+    private function trailers($trailers)
+    {
+        foreach ($trailers as &$trailer) {
+            if ($trailer['type'] == 'youtube') {
+                $trailer['type'] = ucfirst($trailer['type']);
+                $trailer['url'] = 'http://www.youtube.com/watch?v=' . $trailer['id'];
+                $trailer['embedded'] = '<iframe src="//www.youtube.com/embed/' . $trailer['id'] . '" frameborder="0" allowfullscreen></iframe>';
+            }
+
+            if ($trailer['type'] == 'vimeo') {
+                $trailer['type'] = ucfirst($trailer['type']);
+                $trailer['url'] = 'http://www.vimeo.com/' . $trailer['id'];
+                $trailer['embedded'] = '<iframe src="http://player.vimeo.com/video/' . $trailer['id'] . '" frameborder="0" allowfullscreen></iframe>';
+            }
+        }
+
+        return $trailers;
     }
 }
--- test/Presskit/Test/FormatTest.php
+++ test/Presskit/Test/FormatTest.php
@@ -27,4 +27,41 @@ class Presskit_FormatTest extends PHPUnit_Framework_Testcase
         $expected = array('website' => 'http://www.website.com/', 'website-name' => 'website.com');
         $this->assertEquals($expected, $this->format->format($input));
     }
+
+    public function testTrailers()
+    {
+        $input = array(
+            'trailers' => array(
+                array(
+                    'name' => 'Youtube video',
+                    'type' => 'youtube',
+                    'id' => '7jQbITg0MSk',
+                ),
+                array(
+                    'name' => 'Vimeo video',
+                    'type' => 'vimeo',
+                    'id' => '12536488',
+                ),
+            ),
+        );
+        $expected = array(
+            'trailers' => array(
+                array(
+                    'name' => 'Youtube video',
+                    'type' => 'Youtube',
+                    'id' => '7jQbITg0MSk',
+                    'url' => 'http://www.youtube.com/watch?v=7jQbITg0MSk',
+                    'embedded' => '<iframe src="//www.youtube.com/embed/7jQbITg0MSk" frameborder="0" allowfullscreen></iframe>',
+                ),
+                array(
+                    'name' => 'Vimeo video',
+                    'type' => 'Vimeo',
+                    'id' => '12536488',
+                    'url' => 'http://www.vimeo.com/12536488',
+                    'embedded' => '<iframe src="http://player.vimeo.com/video/12536488" frameborder="0" allowfullscreen></iframe>',
+                ),
+            ),
+        );
+        $this->assertEquals($expected, $this->format->format($input));
+    }
 }
